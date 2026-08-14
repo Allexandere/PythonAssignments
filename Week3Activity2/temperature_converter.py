cd27670fe1b9c94edf30bc8e5a214f02fa88b7e7
Allexandere <gamirnev@mail.ru>
2026-08-15 10:02:23 +1200

    minor changes

--- Week3Activity2/temperature_converter.py
+++ Week3Activity2/temperature_converter.py
@@ -1,19 +1,20 @@
 from pathlib import Path
 
+BASE_DIR = Path(__file__).parent
 
-def init():    
-    BASE_DIR = Path(__file__).parent
+def init():
     text_file = BASE_DIR / "junk.txt"
 
     junk_text = text_file.read_text(encoding="utf-8")
-    
+
     print("Total number of lines: " + str(len(junk_text.splitlines())))
-    
+
     junk_text += "text file nanalyssis"
     junk_text = junk_text.upper()
-    
+
     new_text_file = BASE_DIR / "junk2.txt"
     new_text_file.write_text(junk_text, encoding="utf-8")
+
 
 if __name__ == "__main__":
     init()
